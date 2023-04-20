8a4471c17c348fe0450c45b8a306afb0a8c111a6
iputintsev <iputintsev@phoenixit.ru>
2023-04-20 17:10:39 +0300

    Initial commit

---
+++ src/main/java/ru/liga/oldtinder/OldtinderApplication.java
@@ -0,0 +1,13 @@
+package ru.liga.oldtinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldtinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OldtinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/liga/oldtinder/OldtinderApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.liga.oldtinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OldtinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
